51a838cad0b61f8a392de93232eca1499782e661
AgustinPalmaM <palma.agustin7@gmail.com>
2023-01-13 18:20:59 -0300

    Create classes Budget to manage amounts of budget and spents and class UI to manage the needed html to insert data

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 // variables
+
 const form = document.querySelector('#agregar-gasto');
 const spentList = document.querySelector('#gastos ul');
 
 // Events
+
 eventListeners();
 
 function eventListeners() {
@@ -12,19 +14,46 @@ function eventListeners() {
 
 // Classes
 
+class Budget {
+  constructor(budget) {
+    this.budget = Number(budget);
+    this.balance = Number(budget);
+    this.spents = [];
+  }
+}
+
+class UI {
+
+  insertBudget(amounts) {
+    // make const with elements from budget object
+    const { budget, balance } = amounts;
+
+    // insert html with budget and balance
+    document.querySelector('#total').textContent = budget;
+    document.querySelector('#restante').textContent = balance;
+
+  }
+
+}
+
+// Instances
+
+const ui = new UI();
+let budget;
+
+
 // Functions
 
 function askBudget() {
-  const budget = Number(prompt('What is your budget'));
+  const askedBudget = prompt('What is your budget');
   
   
-  if( budget === '' || budget === null || isNaN(budget) || budget <= 0 ) {
+  if( askedBudget === '' || askedBudget === null || isNaN(askedBudget) || askedBudget <= 0 ) {
     alert('You must enter a number and greater than zero')
     window.location.reload();
   } 
 
-  
-  
-
+  budget = new Budget(askedBudget);
+  ui.insertBudget(budget)
 }
 
