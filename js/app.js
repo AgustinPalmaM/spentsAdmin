b66e4791a418011d18a3b622841bd2e7e98f6ee2
AgustinPalmaM <palma.agustin7@gmail.com>
2023-01-13 19:00:05 -0300

    Create function addSpent to manage the name and amount of each spent and insert into the html

--- js/app.js
+++ js/app.js
@@ -8,7 +8,9 @@ const spentList = document.querySelector('#gastos ul');
 eventListeners();
 
 function eventListeners() {
-  document.addEventListener('DOMContentLoaded', askBudget )
+  document.addEventListener('DOMContentLoaded', askBudget );
+
+  form.addEventListener('submit', addSpent );
 }
 
 
@@ -31,6 +33,27 @@ class UI {
     // insert html with budget and balance
     document.querySelector('#total').textContent = budget;
     document.querySelector('#restante').textContent = balance;
+
+  }
+
+  printAlert( message, typeMessage ) {
+    const divMessage = document.createElement('DIV');
+    divMessage.classList.add('text-center', 'alert');
+
+    
+    if (typeMessage === 'error') {
+      divMessage.classList.add('alert-danger');
+    } else {
+      divMessage.classList.add('alert-success');
+    }
+    
+    divMessage.textContent = message;
+    
+    document.querySelector('.primario').insertBefore(divMessage, form);
+    
+    setTimeout(() => {
+      divMessage.remove();
+    }, 1000)
 
   }
 
@@ -57,3 +80,22 @@ function askBudget() {
   ui.insertBudget(budget)
 }
 
+function addSpent(e) {
+  
+  e.preventDefault();
+
+  // read data from form
+  const spentName = document.querySelector('#gasto').value;
+  const spentAmount = document.querySelector('#cantidad').value;
+
+  if (spentName === '' || spentAmount === '') {
+    ui.printAlert('You should enter both fields', 'error')
+    return;
+  } else if ( spentAmount <= 0 || isNaN(spentAmount) ) {
+    ui.printAlert(' Amount must be a number and greater than zero ', 'error' )
+    return;
+  }
+
+  console.log('adding spent')
+
+}
